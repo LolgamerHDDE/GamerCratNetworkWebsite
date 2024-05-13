338eb15ba6e7be2333c4ddcfbc2c19483b866a5c
Linus Dahlem <dahlemlinus@gmail.com>
2024-05-13 21:12:20 +0200

    app bug fixes

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import os
 
 app = FastAPI()
 app.include_router(fastapi_users.get_auth_router(auth_backend), tags=["auth"])
-app.add_api_router(fastapi_users.get_register_router(UserRead, UserCreate), tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), tags=["auth"])
 app.include_router(fastapi_users.get_reset_password_router(), tags=["auth"])
 app.include_router(fastapi_users.get_verify_router(UserRead), tags=["auth"])
 app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), tags=["users"], prefix="/users")
